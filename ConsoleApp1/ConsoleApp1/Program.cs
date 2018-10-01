ab018834d5685cd5d8632f80fb309f30582ff6cd
Hector <hecmartp@gmail.com>
2018-10-01 18:36:12 +0200

    Ejercicios

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,235 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; // librerias//
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+
+namespace HolaMundo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo 2") ;
+            //int num1, num2, result, numRadio;
+            /* Declarar e iniciar variables
+            String cocheRojo = "", cocheAzul = " Audi ";
+            cocheRojo = "Renault";
+                        
+            Console.WriteLine("El coche rojo es : ") ;
+            Console.WriteLine(cocheRojo);
+
+            const double PI=3.14  //Las contantes siempre empiezan con "const" y el tipo de variable , la variable siempre va en mayusculas.
+            
+
             Console.ReadLine() ; 
+            */
+            //programa 1
+            //Console.WriteLine("Hola Héctor");
+            ////programa 2
+            //Console.WriteLine(2 + 5);
+            ////programa 3
+            //Console.WriteLine(9 / 9);
+            ////programa 4
+            //Console.WriteLine(" Programa 4 ");
+            //Console.WriteLine(-5 + 8 * 6);
+            //Console.WriteLine(55 + 9 % 9);
+            //Console.WriteLine(20 + -3 * 5 / 8);
+            //Console.WriteLine(5 + 15 / 3 * 2 - 8 % 3);
+            //Programa 5  que solicite los numeros a los usuarios y que muestre el resultado de multiplicar 2
+
+            //Console.WriteLine("Escribe la primer cifra : ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Escribe la segunda cifra : ");
+            //num2 = Convert.ToInt32(Console.ReadLine());     // Aqui hice una conversion de string a Int
+            //result = num1 * num2;
+            //Console.WriteLine("La multiplicacion entre los dos numeros es : " + num1 +" * "+ num2 + " es " +(num1*num2) );
+
+            //Programa 6  
+
+            //Console.WriteLine("Escribe la primer cifra : ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Escribe la segunda cifra : "); 
+            //num2 = Convert.ToInt32(Console.ReadLine());     // Aqui hice una conversion de string a Int
+
+            //Console.WriteLine("La suma entre los dos numeros es : " + num1 + " + " + num2 + " es " + (num1 + num2));
+            //Console.WriteLine("La resta entre los dos numeros es : " + num1 + " - " + num2 + " es " + (num1 - num2));
+            //Console.WriteLine("La multiplicacion entre los dos numeros es : " + num1 + " * " + num2 + " es " + (num1 * num2));
+            //Console.WriteLine("La divicion entre los dos numeros es : " + num1 + " / " + num2 + " es " + (num1 / num2));
+
+            //Console.ReadLine();
+
+            //int i = 0;
+            //int numUlt;
+            //// Programa 7
+
+            //Console.WriteLine("Escribe El numero : ");
+            //numUlt = Convert.ToInt32(Console.ReadLine());
+            //while (i <11)
+            //{
+
+            //    Console.WriteLine("La multiplicacion de : " + i + " * " + numUlt + " es " + (i*numUlt));
+            //    i++;
+            //}
+            //Console.ReadLine();
+
+
+            //Escribe un programa que calcule el resultado de la siguiente operación: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+            //Console.WriteLine((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+
+            //Console.WriteLine(4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+
+            //Console.ReadLine();
+
+            ////Escribe un programa que pida al usuario el radio de un círculo y que calcule y muestre el área y el perímetro (el área de un círculo es PI por radio cuadrado y el perímetro es 2 por radio por PI). PI es = 3.141593
+            //const double PI = 3.141593;
+            //Console.WriteLine("Escribe el tamaño del radio : ");
+            //numRadio = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("El area del circulo  : " + PI*( Math.Pow(numRadio, 2)));
+            //Console.WriteLine("El diametro del circulo  : " + PI * (numRadio*2));
+
+            //////Escribe un programa que pida tres números al usuario y que calcule y muestre su media.
+            //double numMedia1, numMedia2, numMedia3;
+
+            //Console.WriteLine("Introduce el numero 1");
+            //numMedia1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Introduce el numero 2");
+            //numMedia2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Introduce el numero 3");
+            //numMedia3 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("La media es "+ (numMedia1+numMedia2+numMedia3)/3);
+            //Console.ReadLine();
+
+            ////Escribe un programa que pida al usuario la anchura y altura de un rectángulo y que calcule y muestre su área y su perímetro 
+            ///(el área es la altura por la anchura y el perímetro es 2 por la suma de la altura y la anchura).
+            //    double numBaseT, numAlturaT , numAreaT, numPerimetroT;
+            //    Console.WriteLine("introduce la base");
+            //    numBaseT = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("introduce la altura");
+            //    numAlturaT = Convert.ToDouble(Console.ReadLine());
+            //    numAreaT = (numAlturaT * numBaseT) / 2;
+            //    numPerimetroT = (2*(numAlturaT+numBaseT));
+            //    Console.WriteLine("La medida de la Area es " + numAreaT + "y la medida del perimetro es  " + numPerimetroT);
+
+            //    Console.ReadLine();
+            ////Escribe un programa que pida dos variables, y después intercambie sus valores
+            // double numVari1, numVari2, numVari3;
+
+
+
+            // numVari3 = numVari1;
+            // numVari1 = numVari2;
+            // numVari2 = numVari3;
+
+            // Console.WriteLine("La variable 1 =  " + numVari1 + "   La variable 2 = " + numVari2 );
+
+
+
+            //Console.ReadLine(); 
+
+            //Escribe un programa que pida un número de 3 dígitos y que muestre en pantalla el resultado de sumarlos.
+            //int numSumado , numCent , numDec , numUnid ;
+
+            //Console.WriteLine("Introduce el numero de 3 digitos");
+            //numSumado = Convert.ToInt32(Console.ReadLine());
+            //numCent = (numSumado / 100);
+            //numDec = ((numSumado-(numCent * 100))/10 );
+            //numUnid = ((numSumado - (numCent * 100)) % 10);
+            //Console.WriteLine("El primer digito es " + numCent + " El segundo digito es " + numDec  + " El tercer numero es " + numUnid );
+            //Console.ReadLine(); 
+
+            ////Escribe un programa que pida al usuario un dígito (n) y calcule y muestre el valor de n+nn+nnn
+
+            //int nummultip, nummultipDec, nummultipCent;
+            //Console.WriteLine ("Introduce un numero ");
+            //nummultip=Convert.ToInt32(Console.ReadLine());
+            //nummultipDec =  (nummultip * 10) + nummultip;
+            //nummultipCent = (nummultip * 100 )+ nummultipDec;
+
+            //Console.WriteLine(nummultip + "+" + nummultipDec + "+" + nummultipCent);
+            //Console.ReadLine();
+
+
+
+            ////Escribe un programa que calcule el resto de dividir dos números sin utilizar el operador de división de resto (%)
+            //int numU , numD, resulta , resultres ;
+
+            //Console.WriteLine("introduce num 1 ");
+            //numU = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("introduce num 2");
+            //numD = Convert.ToInt32(Console.ReadLine());
+
+
+            //resulta = numU / numD;
+            //resultres = numU- (resulta * numD) ; 
+            //Console.WriteLine("el resultado de la divicion es  " + resulta + " El residuo es " + resultres);
+            //Console.ReadLine();
+
+            ////Escribe un programa que le pida al usuario un número de pulgadas y lo convierta a metros (una pulgada son 0.0254 metros)
+
+            //const double PULGADAS = 0.0254;
+            //double nPulgadas;
+
+            // Console.WriteLine("introduce las pulgadas ");
+            // nPulgadas= Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("La conversion a metros es : " + nPulgadas*PULGADAS );
+            //Console.ReadLine();
+
+            //// Escribe un programa que pida un número de minutos y muestre cuantos años y días son
+            //Double nMinutos, nD, nM, nY;
+            //Console.WriteLine("Introduce un numero de minutos ");
+            //nMinutos = Convert.ToDouble(Console.ReadLine());
+            //nD = (nMinutos / 60) /24;
+            //nY = nD / 365;
+            //Console.WriteLine("el numero de minutos es  " + nMinutos);
+            //Console.WriteLine("el numero de dias son  " + nD);
+            //Console.WriteLine("el numero de años es  " + nY);
+
+
+            ////Escribe un programa que pida una distancia (en metros), y el tiempo tardado (como tres números: horas, minutos y segundos) y que muestre la velocidad en metros por segundo y kilómetros por hora.
+
+            //double DistM , hr , min , seg , cvMs , cvH , totalseg , mps , kmph ;
+
+            //Console.WriteLine("introduce la distancia en metros ");
+            //DistM = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("introduce las horas");
+            //hr = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("introduce los minutos");
+            //min = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("introduce los segundos");
+            //seg = Convert.ToDouble(Console.ReadLine());
+
+            //cvH = 3600 * hr;
+            //cvMs = 60 * min;
+
+            //totalseg = cvH + cvMs + seg; 
+            // Console.Write(" Las Horas en segundos son : " + cvH );
+            //Console.Write(" Los minutos en segundos son : " + cvMs );
+            //Console.Write(" Los segundos son : " + seg);
+            //Console.Write(" El total de segundos son : " + totalseg);
+            //kmph = ((DistM / 1000) / (totalseg/3600));
+            //Console.ReadLine();
+            //Console.Write(" El total de metros a km/h son : " + kmph);
+            //mps = DistM/totalseg;
+            //kmph = (DistM / 1000);
+
+            //Console.Write(" El total de mps : " + mps);
+
+            //Console.ReadLine();
+
+            ////Crear un numero Random
+
+            //Random rnd = new Random();
+            //int number = rnd.Next(1, 101);
+            //Console.Write(" El numero : " + number);
+
+            //Console.ReadLine();
+
 
         }
     }
