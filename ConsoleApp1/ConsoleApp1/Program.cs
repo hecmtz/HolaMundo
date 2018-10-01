ac71c7e354e899bb54a7566fea214a13655e298a
Hector <hecmartp@gmail.com>
2018-10-01 11:06:21 +0200

    primer comentario

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo") ;
+            Console.WriteLine("Hola mundo 2") ;
             Console.ReadLine() ; 
 
         }
